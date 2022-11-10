851fe8abc75b7d3f073c23f87e42481ca9f6e3e3
Duc Bach <bachtrananhduc@gmail.com>
2022-11-10 13:49:49 +0700

    login

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import "antd/dist/antd.css";
 import "./index.css";
 import App from "./App";
 
+import { AppProvider } from "./context/appContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
